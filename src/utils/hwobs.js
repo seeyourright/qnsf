bca39c1e24805542f2b0289aa2ff2428624a619f
se <asa>
2020-09-02 15:05:13 +0800

    1

--- src/utils/hwobs.js
+++ src/utils/hwobs.js
@@ -1,8 +1,13 @@
 import '../assets/js/esdk-obs-browserjs-es5-3.19.9.min'
+import http from '../api/http'
+
+// http.get('').then(res => {
+// //
+// // })
 
 export default new ObsClient({
-  access_key_id: 'FLOFE7BY96XIRARSVBLF',
-  secret_access_key: '7daciLKdX5Uw3iW0fv9X4UPC16AdSn3UlOKhC5B1',
+  access_key_id: 'FLOFE7BY96XI'+'RARSVBLF',
+  secret_access_key: '7daciLKdX5Uw3iW0f'+'v9X4UPC16AdSn3UlOKhC5B1',
   server : 'http://obs.cn-east-3.myhuaweicloud.com',
   timeout : 60 * 5,
 });
